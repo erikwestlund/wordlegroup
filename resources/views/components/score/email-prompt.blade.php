d6f3216d06ebaf4a6b2c1369981024dbdae9d70d
Erik Westlund <erikwestlund@hey.com>
2022-03-07 16:48:12 -0600

    center the button

--- resources/views/components/score/email-prompt.blade.php
+++ resources/views/components/score/email-prompt.blade.php
@@ -3,7 +3,7 @@
         The easiest way to share a score is to click "Share" on your Wordle Board, select your mail client, and send it off to <a class="link" href="mailto:scores@wordlegroup.com">scores@wordlegroup.com</a>.
     </p>
     <div class="mt-8">
-        <a href="/email/WordleGroup.vcf" role="button" class="inline-flex text-sm font-semibold justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
+        <a href="/email/WordleGroup.vcf" role="button" class="inline-flex items-center text-sm font-semibold justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-500">
             <x-icon-solid.address-card class="w-4 h-4 fill-white mr-4" />
             Add Wordle Group To Your Contacts
         </a>
