918de828550ba312ac4894ea5eb066830c4d13ff
Erik Westlund <erikwestlund@hey.com>
2022-03-07 16:45:48 -0600

    add access

--- app/Providers/HorizonServiceProvider.php
+++ app/Providers/HorizonServiceProvider.php
@@ -35,7 +35,8 @@ class HorizonServiceProvider extends HorizonApplicationServiceProvider
     {
         Gate::define('viewHorizon', function ($user) {
             return in_array($user->email, [
-                'edbwestlund@gmail.com'
+                'edbwestlund@gmail.com',
+                'erikwestlund@hey.com',
             ]);
         });
     }
