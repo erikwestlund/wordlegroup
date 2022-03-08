d1e1df31050561f397c42df5bf11b3eff164a901
Erik Westlund <erikwestlund@hey.com>
2022-03-07 19:55:37 -0600

    order

--- resources/views/livewire/account/home.blade.php
+++ resources/views/livewire/account/home.blade.php
@@ -23,23 +23,22 @@
                         />
                     </div>
                 </div>
-
-                <div class="pt-8">
-                    <x-layout.sub-heading class="text-center">My Groups</x-layout.sub-heading>
-                    <div class="mt-8">
-                        <x-account.groups-list :user="$user"/>
-                    </div>
-                    <div class="mt-8 text-center">
-                        <a
-                            class="text-sm inline-flex items-center rounded-lg bg-white border border-gray-200 hover:bg-gray-50 hover:border-gray-300 px-4 py-2"
-                            href="{{ route('group.create') }}"
-                        >
-                            <x-icon-regular.plus class="h-3 w-3 mr-1.5 fill-gray-500"/>
-                            Create A New Group</a>
-                    </div>
-                </div>
-
             @endunless
+                
+            <div class="pt-8">
+                <x-layout.sub-heading class="text-center">My Groups</x-layout.sub-heading>
+                <div class="mt-8">
+                    <x-account.groups-list :user="$user"/>
+                </div>
+                <div class="mt-8 text-center">
+                    <a
+                        class="text-sm inline-flex items-center rounded-lg bg-white border border-gray-200 hover:bg-gray-50 hover:border-gray-300 px-4 py-2"
+                        href="{{ route('group.create') }}"
+                    >
+                        <x-icon-regular.plus class="h-3 w-3 mr-1.5 fill-gray-500"/>
+                        Create A New Group</a>
+                </div>
+            </div>
             <div class="pt-8">
                 <x-layout.sub-heading class="text-center">Record A Score</x-layout.sub-heading>
                 @if($user->daily_scores_recorded === 0)
