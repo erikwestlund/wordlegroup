e56e56036469621fd2ff72062dbffa474ad89f55
Erik Westlund <erikwestlund@hey.com>
2022-03-11 06:46:51 -0600

    leaderboard tuen up

--- resources/views/components/group/leaderboard.blade.php
+++ resources/views/components/group/leaderboard.blade.php
@@ -15,46 +15,46 @@
                     <tbody>
                     @foreach($group->leaderboard as $position)
                     <tr class="bg-white">
-                        <td class="text-center pl-1 pr-3 py-1 sm:px-4 sm:py-2 whitespace-nowrap
+                        <td class="text-center pl-1 pr-3 py-1 sm:px-4 sm:py-2 whitespace-nowrap @if(!isset($group->leaderboard[$loop->index + 1]['place']) || $group->leaderboard[$loop->index + 1]['place'] != $position['place']) border-b border-gray-100 @endif">
                             @if($position['place'] === 1)
-                            text-base sm:text-xl font-bold bg-gold text-black
+                                <span class="h-12 w-12 inline-flex items-center justify-center text-lg bg-gold text-black font-extrabold rounded-full">{{ $position['place'] }}</span>
                             @elseif($position['place'] === 2)
-                            text-sm sm:text-lg font-semibold bg-silver text-gray-800
+                                <span class="h-10 w-10 inline-flex items-center justify-center text-lg bg-silver text-black font-bold rounded-full">{{ $position['place'] }}</span>
                             @elseif($position['place'] === 3)
-                            text-sm sm:text-sm font-semibold bg-bronze text-orange-50
+                                <span class="h-8 w-8 inline-flex items-center justify-center text-lg bg-bronze text-black font-bold rounded-full">{{ $position['place'] }}</span>
                             @else
-                            text-xs font-medium text-gray-900
+                                <span class="font-semibold">{{ $position['place'] }}</span>
                             @endif
-                        ">{{ $position['place'] }}</td>
-                        <td class="px-1 py-1 sm:px-4 sm:py-2 whitespace-nowrap truncate
+                        </td>
+                        <td class="px-1 py-1 sm:px-4 sm:py-2 whitespace-nowrap truncate @if(!isset($group->leaderboard[$loop->index + 1]['place']) || $group->leaderboard[$loop->index + 1]['place'] != $position['place']) border-b border-gray-100 @endif
                             @if($position['place'] === 1)
-                            text-base sm:text-xl font-bold bg-gold text-black
+                            text-base sm:text-xl font-bold
                             @elseif($position['place'] === 2)
-                            text-sm sm:text-lg font-bold sm:font-bold bg-silver text-gray-800
+                            text-sm sm:text-lg font-bold sm:font-bold
                             @elseif($position['place'] === 3)
-                            text-sm sm:text-sm font-semibold bg-bronze text-orange-50
+                            text-sm sm:text-sm font-bold
                             @else
                             text-sm sm:text-sm text-gray-900
                             @endif
                         " title="{{ $position['name'] }}">{{ $position['name'] }}</td>
-                        <td class="px-2 py-1 sm:py-2 whitespace-nowrap text-right
+                        <td class="px-2 py-1 sm:py-2 whitespace-nowrap text-right @if(!isset($group->leaderboard[$loop->index + 1]['place']) || $group->leaderboard[$loop->index + 1]['place'] != $position['place']) border-b border-gray-100 @endif
                             @if($position['place'] === 1)
-                            text-base sm:text-xl font-bold bg-gold text-black
+                            text-base sm:text-xl font-bold
                             @elseif($position['place'] === 2)
-                            text-sm sm:text-lg font-bold sm:font-bold bg-silver text-gray-800
+                            text-sm sm:text-lg font-bold sm:font-bold
                             @elseif($position['place'] === 3)
-                            text-sm sm:text-sm font-semibold bg-bronze text-orange-50
+                            text-sm sm:text-sm font-bold
                             @else
                             text-sm sm:text-sm text-gray-900
                             @endif
                         ">{!! $position['stats']['mean'] ?: "&#x2014;" !!}</td>
-                        <td class="px-2 py-1 sm:py-2 whitespace-nowrap  text-right
+                        <td class="px-2 py-1 sm:py-2 whitespace-nowrap  text-right @if(!isset($group->leaderboard[$loop->index + 1]['place']) || $group->leaderboard[$loop->index + 1]['place'] != $position['place']) border-b border-gray-100 @endif
                             @if($position['place'] === 1)
-                            text-base sm:text-xl font-bold bg-gold text-black
+                            text-base sm:text-xl font-bold
                             @elseif($position['place'] === 2)
-                            text-sm sm:text-lg font-bold sm:font-bold bg-silver text-gray-800
+                            text-sm sm:text-lg font-bold sm:font-bold
                             @elseif($position['place'] === 3)
-                            text-sm sm:text-sm font-semibold bg-bronze text-orange-800
+                            text-sm sm:text-sm font-bold
                             @else
                             text-sm sm:text-sm text-gray-900
                             @endif
