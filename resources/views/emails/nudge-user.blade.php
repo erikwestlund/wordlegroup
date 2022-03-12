4f6e1888e654a014fbd879574ccd7de84f4d8c9a
Erik Westlund <erikwestlund@hey.com>
2022-03-12 10:55:42 -0600

    language

--- resources/views/emails/nudge-user.blade.php
+++ resources/views/emails/nudge-user.blade.php
@@ -1,9 +1,9 @@
 @component('mail::message')
 # You Have Been Nudged By {{ $nudgedByUser->name }}
 
-{{ $nudgedByUser->name }} nudged you to record your Wordle Scores.
+{{ $nudgedByUser->name }} has nudged you to record your Wordle scores to [Wordle Group][{{ route('home') }}.
 
-You can also your scores to [scores@wordlegroup.com](mailto:scores@wordlegroup.com).  Just click "Share" on your Wordle Screen, select your email client, and send your board to scores@wordlegroup.com. We'll take care of the rest.
+You can also your scores to record them.  Just click "Share" on your Wordle Screen, select your email client, and send your board to [scores@wordlegroup.com](mailto:scores@wordlegroup.com). We'll take care of the rest.
 
 Alternatively, click the below button to visit the record scores page.
 
@@ -13,7 +13,7 @@ Record My Scores
 
 If you are having trouble clicking the above button, you can visit your account home page at the below URL:
 
-{{ route('account.home') }}
+[{{ route('account.home') }}]({{ route('account.home') }})
 
 If you would not like to receive reminder emails anymore, [update your account settings]({{ route('account.settings') }}).
 
