68c3e7c97d538da7faea317c76dee58b3e599e3f
Erik Westlund <erikwestlund@hey.com>
2022-03-12 21:18:16 -0600

    mobile chart

--- resources/views/components/score/bar-chart.blade.php
+++ resources/views/components/score/bar-chart.blade.php
@@ -7,102 +7,104 @@
     @endonce
 
     <div
+        class="h-72 sm:h-auto"
         x-data="{
-    labels: ['1', '2', '3', '4', '5', '6', 'Missed'],
-    values: @json($values),
-    init() {
-        Chart.register(ChartDataLabels);
+                labels: ['1', '2', '3', '4', '5', '6', 'Missed'],
+                values: @json($values),
+                init() {
+                Chart.register(ChartDataLabels);
 
 
-        let chart = new Chart(this.$refs.canvas.getContext('2d'), {
-            type: 'bar',
-            data: {
-                labels: this.labels,
-                datasets: [{
-                    data: this.values,
-                    backgroundColor: '#16803C',
-                    borderColor: '#16803C',
-                }],
-            },
-            options: {
-                base: 0,
-                indexAxis: 'y',
-                interaction: { intersect: false },
-                layout: { padding: { right: 20 } },
-                scales: {
-                    y: {
-                        min: 0,
-                        grid: {
-                            display: false,
-                        },
-                        ticks: {
-                            font: {
-                                size: 14,
-                                weight: 500,
-                            },
-                        },
-                         title: {
-                            display: false,
-                            text: 'Score'
-                          }
+                let chart = new Chart(this.$refs.canvas.getContext('2d'), {
+                    type: 'bar',
+                    data: {
+                        labels: this.labels,
+                        datasets: [{
+                            data: this.values,
+                            backgroundColor: '#16803C',
+                            borderColor: '#16803C',
+                        }],
                     },
-                    x: {
-                        beginAtZero: true,
-                        grid: {
-                            display: false,
-                        },
-                        ticks: {
-                            stepSize: {{ $stepSize }},
-                            font: {
-                                size: 14,
-                                weight: 500,
+                    options: {
+                        base: 0,
+                        maintainAspectRatio: false,
+                        indexAxis: 'y',
+                        interaction: { intersect: false },
+                        layout: { padding: { right: 20 } },
+                        scales: {
+                            y: {
+                                min: 0,
+                                grid: {
+                                    display: false,
+                                },
+                                ticks: {
+                                    font: {
+                                        size: 14,
+                                        weight: 500,
+                                    },
+                                },
+                                 title: {
+                                    display: false,
+                                    text: 'Score'
+                                  }
                             },
-{{--                            color: '#C8B458',--}}
-                        },
-                         title: {
-                            display: true,
-                            text: 'Count'
-                      }
-                    }
-                 },
-                plugins: {
-                    datalabels: {
-                        color: 'black',
-                        anchor: 'end',
-                        align: 'right',
-                        labels: {
-                            title: {
-                                font: {
-                                    weight: 400,
+                            x: {
+                                beginAtZero: true,
+                                grid: {
+                                    display: false,
+                                },
+                                ticks: {
+                                    stepSize: {{ $stepSize }},
+                                    font: {
+                                        size: 14,
+                                        weight: 500,
+                                    },
+        {{--                            color: '#C8B458',--}}
+                                },
+                                 title: {
+                                    display: true,
+                                    text: 'Count'
+                              }
+                            }
+                         },
+                        plugins: {
+                            datalabels: {
+                                color: 'black',
+                                anchor: 'end',
+                                align: 'right',
+                                labels: {
+                                    title: {
+                                        font: {
+                                            weight: 400,
+                                        }
+                                    },
                                 }
                             },
-                        }
-                    },
-                    legend: {
-                        display: false,
-                    },
-                    tooltip: {
-                        enabled: false,
-                        displayColors: false,
-                        callbacks: {
-                            label(point) {
-                                return 'Scores: '+point.raw
+                            legend: {
+                                display: false,
+                            },
+                            tooltip: {
+                                enabled: false,
+                                displayColors: false,
+                                callbacks: {
+                                    label(point) {
+                                        return 'Scores: '+point.raw
+                                    }
+                                }
                             }
                         }
                     }
-                }
+                })
+
+
+                this.$watch('values', () => {
+                    chart.data.labels = this.labels
+                    chart.data.datasets[0].data = this.values
+
+                    chart.update()
+                })
             }
-        })
-
-
-        this.$watch('values', () => {
-            chart.data.labels = this.labels
-            chart.data.datasets[0].data = this.values
-
-            chart.update()
-        })
-    }
-}"
+        }"
     >
         <canvas x-ref="canvas"></canvas>
     </div>
