bf20eab5aa8c14352aca28b0d4477b3fbde825ce
Erik Westlund <erikwestlund@hey.com>
2022-03-19 21:43:07 -0500

    share buttons

--- resources/views/components/score/share-link-button.blade.php
+++ resources/views/components/score/share-link-button.blade.php
@@ -2,9 +2,9 @@
     role="button"
     {{ $attributes->merge(['title' => 'Copy Share Link']) }}
     @if($buttonClass)
-    class="{{ $buttonClass }}"
+    class="block {{ $buttonClass }}"
     @else
-    class="px-1 py-1 mr-2 text-xs text-gray-400 rounded border border-gray-200 last:mr-0 hover:border-wordle-yellow hover:text-wordle-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-700"
+    class="block px-1 py-1 mr-2 text-xs text-gray-400 rounded border border-gray-200 last:mr-0 hover:border-wordle-yellow hover:text-wordle-yellow focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-green-700"
     @endif
     x-data
     @if($confirm)
