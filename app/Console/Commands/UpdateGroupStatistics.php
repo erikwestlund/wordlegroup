9f27cb41b28201ce13a8e3f8126677e369adf90e
Erik Westlund <erikwestlund@hey.com>
2022-06-20 14:00:07 -0400

    update stats command

---
+++ app/Console/Commands/UpdateGroupStatistics.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Group;
+use Illuminate\Console\Command;
+
+class UpdateGroupStatistics extends Command
+{
+    protected $signature = 'groups:update-stats';
+
+    protected $description = 'Update group statistics.';
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function handle()
+    {
+        Group::all()
+             ->each(function ($group) {
+                 $group->updateStats();
+             });
+
+        return 1;
+    }
+}
